ee0ddb2fd3dc842220f760d7792449adceb58b02
Dushyant <52059485+Dushyant-coder@users.noreply.github.com>
2023-10-06 01:27:54 +0530

    Add files via upload

---
+++ Square root.py
@@ -0,0 +1,7 @@
+#Area of the triangle
+
+
+height= float(input("Enter the height of the triangle")) #input the height of the triangle
+base= float(input("Enter the base of the triangle")) #input the base of the triangle
+area= 0.5*base*height #formula of the area of the triangle
+print("area of the triangle",area) #printed the value of the area after the calculation
